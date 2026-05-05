841275be0064933718e889feaca056b5a9b0476c
Bauyrjan18 <baukabalkybek@gmail.com>
2026-05-05 22:04:30 +0500

    1

--- src/com/narxoz/rpg/quest/OrderedQuestIterator.java
+++ src/com/narxoz/rpg/quest/OrderedQuestIterator.java
@@ -1,11 +1,8 @@
 package com.narxoz.rpg.quest;
 
 import java.util.List;
-
-/**
- * Traverses quests in arrival order.
- */
-public class OrderedQuestIterator implements QuestIterator {
+import java.util.NoSuchElementException;
+ class OrderedQuestIterator implements QuestIterator {
 
     private final List<Quest> snapshot;
     private int cursor;
@@ -17,13 +14,14 @@ public class OrderedQuestIterator implements QuestIterator {
 
     @Override
     public boolean hasNext() {
-        // TODO: return true while the cursor still points at an unread quest.
-        return false;
+        return cursor < snapshot.size();
     }
 
     @Override
     public Quest next() {
-        // TODO: return the current quest and advance the cursor.
-        return null;
+        if (!hasNext()) {
+            throw new NoSuchElementException("No more quests in the log.");
+        }
+        return snapshot.get(cursor++);
     }
 }
